795666b0ad8acdffecde00e3d1c188e08842cba6
I-BroneX-I <vladislav.2022.markevich@gmail.com>
2023-12-23 12:50:09 +0200

    create topNav

--- src/scripts/hello.js
+++ src/scripts/hello.js
@@ -1,1 +1,44 @@
-console.log('Hello');
+'use strict'
+
+const topNav = document.querySelector('#topNav');
+const navContainer = document.querySelector('#navContainer');
+const login = document.querySelector('#login');
+const burgerBtn = document.querySelector('#burger');
+const hrs = document.querySelectorAll('#hrBurger');
+
+function observerWidth() {
+    console.log(window.innerWidth);
+    if (window.innerWidth > 769) {
+        topNav.classList.remove('activeTopNav');
+        navContainer.classList.remove('activeNavContainer');
+        login.classList.remove('activeLogin');
+        topNav.classList.remove('closeTopNav');
+    }
+
+    topNav.classList.remove('activeTopNav');
+    navContainer.classList.remove('activeNavContainer');
+    login.classList.remove('activeLogin');
+
+    for (const hr of hrs) {
+        hr.classList.remove('activeHr');
+    }
+}
+
+function toggleNav() {
+    topNav.classList.toggle('activeTopNav');
+    navContainer.classList.toggle('activeNavContainer');
+    login.classList.toggle('activeLogin');
+
+    if(!topNav.classList.contains('activeTopNav')){
+        topNav.classList.add('closeTopNav');
+    } else {
+        topNav.classList.remove('closeTopNav');
+    }
+
+    for (const hr of hrs) {
+        hr.classList.toggle('activeHr');
+    }
+}
+
+window.addEventListener('resize', observerWidth);
+burgerBtn.addEventListener('click', toggleNav);
